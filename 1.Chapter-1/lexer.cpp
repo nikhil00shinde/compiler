f1fb93b91886c21724db8fa2db01ee5341e4194c
nikhil00shinde <nikhishinde00@outlook.com>
2024-12-28 19:55:53 -0500

    lexer code completed

--- 1.Chapter-1/lexer.cpp
+++ 1.Chapter-1/lexer.cpp
@@ -23,19 +23,55 @@ static int gettok() {
 	//Skip any whitespace
 	while (isspace(LastChar))
 		LastChar = getchar();
-}
 
 
 
-if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]
-	Identifier = LastChar
+
+	if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]
+		Identifier = LastChar
 	
-	while (isalnum((LastChar = getchar())))
-		Identifier += LastChar;
+		while (isalnum((LastChar = getchar())))
+			Identifier += LastChar;
 
-	if (IdentifierStr == "def")
-		return tok_def;
-	if (IdentifierStr == "extern")
-		return tok_extern;
-	return tok_identifier;
+		if (IdentifierStr == "def")
+			return tok_def;
+		if (IdentifierStr == "extern")
+			return tok_extern;
+		return tok_identifier;
+	}
+
+
+	if (isdigit(LastChar) || LastChar == '.') { // Number: [0-9.]+
+		std::string NumStr;
+
+		do{
+			NumStr += LastChar;
+			LastChar = getchar();
+		} while (isdigit(LastChar) || LastChar == '.');
+
+		NumVal = strtod(NumStr.c_str(), 0);
+		return tok_number;
+	}
+
+
+	if (LastChar == '#') {
+		// Comment until end of line.
+	
+		do
+			LastChar = getchar();
+		while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+		if (LastChar != EOF)
+			return gettok();
+	}
+
+
+
+	// Check for end of file. Don't eat the EOF
+	if (LastChar == EOF) return tok_eof;
+
+	// Otherwise, just return the character as its ascii value.
+	int ThisChar = LastChar;
+	LastChar = getchar();
+	return ThisChar;
 }
